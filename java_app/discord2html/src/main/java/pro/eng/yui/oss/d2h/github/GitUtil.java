e57b2efff8b75de44b263bff3bbf5dbb78fb117a
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-14 21:18:27 +0900

    GitUtilクラスをconfig活用のためリファクタ

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/github/GitUtil.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/github/GitUtil.java
@@ -1,63 +1,78 @@
 package pro.eng.yui.oss.d2h.github;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Gitコマンドを実行するためのユーティリティクラス。
  * リポジトリに対するfetch、add、commit、pushの基本的なGit操作をJavaから実行できる
  */
+@Component //singleton
 public class GitUtil {
 
-    private GitUtil(){
-        /* utility class */ 
+    private GitConfig config;
+    
+    @Autowired
+    public GitUtil(GitConfig gitConfig){
+        config = gitConfig;
     }
     
     /**
      * fetch
      */
-    public static void fetch(File repoDir) throws Exception {
-        runCommand(repoDir, "git", "fetch");
+    public void fetch() throws Exception {
+        runGitCommand("fetch");
     }
 
     /**
      * リベースでリモート追従
      */
-    public static void pullRebase(File repoDir, String remote, String branch) throws Exception {
-        runCommand(repoDir, "git", "pull", "--rebase", remote, branch);
+    public void pullRebase() throws Exception {
+        final String remote = config.getRepo().getOwner()+"/"+config.getRepo().getName();
+        final String branch = config.getRepo().getMain();
+        runGitCommand("pull", "--rebase", remote, branch);
     }
 
     /**
      * ファイルをadd
      */
-    public static void add(File repoDir, List<String> filePaths) throws Exception {
+    public void add(List<String> filePaths) throws Exception {
         for (String path : filePaths) {
-            runCommand(repoDir, "git", "add", path);
+            runGitCommand("add", path);
         }
     }
 
     /**
      * コミット
      */
-    public static void commit(File repoDir, String message) throws Exception {
-        runCommand(repoDir, "git", "commit", "-m", message);
+    public void commit(String message) throws Exception {
+        runGitCommand("commit", "-m", message);
     }
 
     /**
      * push
      */
-    public static void push(File repoDir, String remote, String branch) throws Exception {
-        runCommand(repoDir, "git", "push", remote, branch);
+    public void push() throws Exception {
+        final String remote = config.getRepo().getOwner()+"/"+config.getRepo().getName();
+        final String branch = config.getRepo().getMain();
+        runGitCommand("push", remote, branch);
     }
 
     /**
-     * 汎用コマンド実行
+     * 汎用gitコマンド実行
      */
-    private static void runCommand(File workDir, String... command) throws Exception {
-        ProcessBuilder pb = new ProcessBuilder(command);
-        pb.directory(workDir);
+    private void runGitCommand(String... command) throws Exception {
+        String[] gitCommand = new String[command.length + 1];
+        gitCommand[0] = "git";
+        System.arraycopy(command, 0, gitCommand, 1, command.length);
+        ProcessBuilder pb = new ProcessBuilder(gitCommand);
+        pb.directory(new File(config.getLocal().getDir()));
         pb.redirectErrorStream(true);
         Process process = pb.start();
 
