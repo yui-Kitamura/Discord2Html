35589486417abb831c794dd7046bb103cbbe90e3
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-15 20:42:36 +0900

    Revert "localhostから127..1に変更"
    This reverts commit 490beb7a

--- docs/deploy/db/01_create_schema.sql
+++ docs/deploy/db/01_create_schema.sql
@@ -2,9 +2,9 @@
 CREATE SCHEMA IF NOT EXISTS discord2html DEFAULT CHARACTER SET utf8mb4;
 
 -- データベースユーザの作成（パスワードはプレースホルダ）
-CREATE USER IF NOT EXISTS 'discord2html_admin'@'127.0.0.1' 
+CREATE USER IF NOT EXISTS 'discord2html_admin'@'localhost' 
     IDENTIFIED BY '{{YOUR_PASSWORD_HERE}}';
 
 -- 作成したユーザに権限を付与
-GRANT ALL PRIVILEGES ON discord2html.* TO 'discord2html_admin'@'127.0.0.1';
+GRANT ALL PRIVILEGES ON discord2html.* TO 'discord2html_admin'@'localhost';
 FLUSH PRIVILEGES;
