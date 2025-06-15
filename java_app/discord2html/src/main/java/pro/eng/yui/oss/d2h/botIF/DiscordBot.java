cb9eefe63f0427dce52cf1df7ce6205372045b2a
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-15 10:40:53 +0900

    botのオンライン状態を形成

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordBot.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordBot.java
@@ -1,16 +1,20 @@
 package pro.eng.yui.oss.d2h.botIF;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 import pro.eng.yui.oss.d2h.config.Secrets;
+
+import java.util.EnumSet;
 
 @Component
 public class DiscordBot extends ListenerAdapter {
@@ -25,9 +29,17 @@ public class DiscordBot extends ListenerAdapter {
     public static void main(String[] args) throws Exception {
         ConfigurableApplicationContext context = SpringApplication.run(DiscordBot.class, args);
         DiscordBot bot = context.getBean(DiscordBot.class);
-        JDABuilder.createDefault(bot.secrets.getDiscordToken())
+        JDA jda = JDABuilder.create(
+                    bot.secrets.getDiscordToken(),
+                        EnumSet.of(
+                                GatewayIntent.GUILD_MEMBERS,
+                                GatewayIntent.GUILD_MESSAGES,
+                                GatewayIntent.MESSAGE_CONTENT
+                        )
+                )
                 .addEventListeners(bot)
                 .build();
+        jda.awaitReady();
     }
 
     @Override
