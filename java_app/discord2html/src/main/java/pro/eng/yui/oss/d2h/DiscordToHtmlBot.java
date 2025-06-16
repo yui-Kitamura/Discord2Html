4281d4db24d39f1383e3c11d4c6676a81b6f56fd
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-16 14:03:33 +0900

    Spring MVC構成ファイルの追加

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/DiscordToHtmlBot.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/DiscordToHtmlBot.java
@@ -2,8 +2,10 @@ package pro.eng.yui.oss.d2h;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:spring.xml")
 public class DiscordToHtmlBot {
     public static void main(String[] args) {
         SpringApplication.run(DiscordToHtmlBot.class, args);
