7be695d043de6bc7153cfb47dbe218d16c0b5e1b
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-16 14:26:44 +0900

    冗長な処理の削除

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/DiscordToHtmlBot.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/DiscordToHtmlBot.java
@@ -2,10 +2,8 @@ package pro.eng.yui.oss.d2h;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource("classpath:spring.xml")
 public class DiscordToHtmlBot {
     public static void main(String[] args) {
         SpringApplication.run(DiscordToHtmlBot.class, args);
