7ead51974f62f0bc7dbebdc173774ab6da93cf93
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-17 10:57:16 +0900

    DB discord_oauth_token のDTO作成

---
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/model/DiscordOauthToken.java
@@ -0,0 +1,20 @@
+package pro.eng.yui.oss.d2h.db.model;
+
+import pro.eng.yui.oss.d2h.db.field.*;
+
+public class DiscordOauthToken {
+    
+    private SeqId seq_id;
+    private UserId user_id;
+    private AccessToken access_token;
+    private RefreshToken refresh_token;
+    private TokenType token_type;
+    private Scope scope;
+    private ExpireAt expire_at;
+    private IssuedAt issued_at;
+    private CreatedAt created_at;
+    private UpdatedAt updated_at;
+    
+    
+    
+}
