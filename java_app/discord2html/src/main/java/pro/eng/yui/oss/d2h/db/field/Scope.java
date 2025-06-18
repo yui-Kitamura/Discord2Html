a636dde22b025dae8890c6faa87bed0cf6320070
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-18 13:13:08 +0900

    戻りの型に対応

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/field/Scope.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/field/Scope.java
@@ -1,8 +1,14 @@
 package pro.eng.yui.oss.d2h.db.field;
 
+import java.util.Set;
+
 public class Scope extends AbstText {
     
     public Scope(String value){
         super(value);
+    }
+    
+    public Scope(Set<String> scopes){
+        super(String.join("+", scopes));
     }
 }
