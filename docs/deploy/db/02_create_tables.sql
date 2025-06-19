ec77e779f5e3433c8da548ba14f447c2eed8a9e6
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-19 20:40:05 +0900

    ID最大桁数配慮

--- docs/deploy/db/02_create_tables.sql
+++ docs/deploy/db/02_create_tables.sql
@@ -6,21 +6,21 @@ CREATE TABLE channels (
 );
 
 CREATE TABLE users (
-    user_id     BIGINT PRIMARY KEY,
+    user_id     BIGINT UNSIGNED PRIMARY KEY,
     user_name   VARCHAR(255) NOT NULL,
     nickname    VARCHAR(255),
     avatar      VARCHAR(255)
 );
 
 CREATE TABLE roles (
-    role_id      BIGINT PRIMARY KEY,
+    role_id      BIGINT UNSIGNED PRIMARY KEY,
     admin_flg    BOOLEAN DEFAULT FALSE,
     anon_flg     BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE user_role (
-    user_id BIGINT NOT NULL,
-    role_id BIGINT NOT NULL,
+    user_id BIGINT UNSIGNED NOT NULL,
+    role_id BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (user_id, role_id),
     FOREIGN KEY (user_id) REFERENCES users(user_id),
     FOREIGN KEY (role_id) REFERENCES roles(role_id)
