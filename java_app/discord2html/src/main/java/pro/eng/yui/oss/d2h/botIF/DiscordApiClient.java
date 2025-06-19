9a27fb3035c8f749d882bcbb27b1a7886d161fdd
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-19 20:47:09 +0900

    DiscordAPIをcallするi

---
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordApiClient.java
@@ -0,0 +1,46 @@
+package pro.eng.yui.oss.d2h.botIF;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import pro.eng.yui.oss.d2h.db.field.AccessToken;
+import pro.eng.yui.oss.d2h.db.field.UserId;
+
+import java.util.Map;
+
+@Service
+public class DiscordApiClient {
+
+    private static final String DISCORD_API_BASE = "https://discord.com/api";
+
+    private final RestTemplate restTemplate;
+
+    public DiscordApiClient() {
+        this.restTemplate = new RestTemplate();
+    }
+
+    protected <T> T get(String endpoint, AccessToken accessToken, Class<T> responseType) {
+        String url = DISCORD_API_BASE + endpoint;
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(accessToken.getValue());
+        headers.set("Accept", "application/json");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<T> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                entity,
+                responseType
+        );
+        return response.getBody();
+    }
+    
+    public UserId fetchUserId(AccessToken token){
+        Map userInfo = get("/users/@me", token, Map.class);
+        return new UserId(Long.parseUnsignedLong(userInfo.get("id").toString()));
+    }
+
+}
