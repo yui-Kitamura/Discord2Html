1bd386b88e510bb37789adcac948359d91ce3f1b
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-24 16:27:25 +0900

    画像URL取得メソッド追加

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/field/Avatar.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/field/Avatar.java
@@ -1,11 +1,23 @@
 package pro.eng.yui.oss.d2h.db.field;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Avatar extends AbstVarChar {
     
+    public static String IMG_HOST = "https://cdn.discordapp.com/avatars";
     public static int LIMIT = 255;
     
     public Avatar(String value){
         super(value, LIMIT);
+    }
+    
+    public URL getImgPath(UserId userId){
+        try {
+            return new URL(IMG_HOST + "/" + userId.toString() + "/" + this.value + ".png");
+        }catch(MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
