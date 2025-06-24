a3067dcfb68056092cf59dc1914820c40ca815b6
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-24 11:05:16 +0900

    Utilメソッドの追加

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/field/Scope.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/field/Scope.java
@@ -1,14 +1,22 @@
 package pro.eng.yui.oss.d2h.db.field;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Scope extends AbstText {
+    
+    protected static String DELIMITER = " ";
     
     public Scope(String value){
         super(value);
     }
     
     public Scope(Set<String> scopes){
-        super(String.join(" ", scopes));
+        super(String.join(DELIMITER, scopes));
+    }
+
+    public Set<String> getScopeSet(){
+        return new HashSet<>(Arrays.asList(getValue().split(DELIMITER)));
     }
 }
