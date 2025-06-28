cb3c56ed797618977fc31dc6a76c93707002364a
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-28 17:39:48 +0900

    import整理

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordBot.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordBot.java
@@ -13,7 +13,6 @@ import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEve
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.requests.restaction.MessageCreateAction;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
