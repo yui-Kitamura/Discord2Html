fa692a40e9965c9c52b81eed7fed1bc76f1254fa
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-06-29 20:33:57 +0900

    ChannelテーブルI/F dao

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/dao/ChannelsDAO.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/db/dao/ChannelsDAO.java
@@ -2,6 +2,8 @@ package pro.eng.yui.oss.d2h.db.dao;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.eng.yui.oss.d2h.consts.exception.DbRecordNotFoundException;
+import pro.eng.yui.oss.d2h.db.field.ChannelId;
 import pro.eng.yui.oss.d2h.db.field.GuildId;
 import pro.eng.yui.oss.d2h.db.mapper.ChannelsMapper;
 import pro.eng.yui.oss.d2h.db.model.Channels;
@@ -28,5 +30,37 @@ public class ChannelsDAO {
         }
         return mapper.selectChannelArchiveDo(param);
     }
+    
+    public Channels selectChannelInfo(ChannelId channelId){
+        Channels param = new Channels();
+        try {
+            param.setChannelId(Objects.requireNonNull(channelId));
+        }catch(NullPointerException npe) {
+            throw new IllegalArgumentException(npe);
+        }
+        Channels res = mapper.selectOne(param);
+        if(res == null) {
+            throw new DbRecordNotFoundException("not found with " + channelId);
+        }
+        return res;
+    }
+    
+    public boolean exists(ChannelId id){
+        try {
+            Channels info = selectChannelInfo(id);
+            return id.equals(info.getChannelId()); //念のため
+        }catch(IllegalArgumentException | DbRecordNotFoundException err) {
+            return false;
+        }
+    }
+    
+    public void upsertChannelInfo(Channels newRecord){
+        if(exists(newRecord.getChannelId())) {
+            mapper.update(newRecord);
+        }else{
+            mapper.register(newRecord);
+        }
+    }
+    
     
 }
