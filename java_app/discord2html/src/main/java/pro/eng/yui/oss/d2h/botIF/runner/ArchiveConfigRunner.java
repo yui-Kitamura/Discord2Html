c556230171f3d9bbc715daabdf35e97b70a9cf1c
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-07-07 14:26:37 +0900

    work #29 on/offの切替実装

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/runner/ArchiveConfigRunner.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/runner/ArchiveConfigRunner.java
@@ -2,20 +2,44 @@ package pro.eng.yui.oss.d2h.botIF.runner;
 
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import pro.eng.yui.oss.d2h.db.dao.ChannelsDAO;
+import pro.eng.yui.oss.d2h.db.field.ChannelId;
+import pro.eng.yui.oss.d2h.db.field.Status;
 
 import java.util.List;
 
 @Component
 public class ArchiveConfigRunner implements IRunner {
     
-    public ArchiveConfigRunner(){
-        // nothing to do
+    private final ChannelsDAO channelDao;
+    
+    @Autowired
+    public ArchiveConfigRunner(ChannelsDAO c){
+        this.channelDao = c;
     }
     
     public void run(Member member, List<OptionMapping> options){
         //TODO set archive settings with new value
         
+        ChannelId targetCh = null;
+        Status newMode = null;
+        for(OptionMapping op : options) {
+            if("target".equals(op.getName())) {
+                 targetCh = new ChannelId(op.getAsChannel());
+                 continue;
+            }
+            if("mode".equals(op.getName())) {
+                newMode = new Status(String.valueOf(op.getAsBoolean()));
+            }
+        }
+        
+        if(targetCh != null && newMode != null) {
+            channelDao.updateChannelStatus(targetCh, newMode);
+        }else {
+            throw new IllegalArgumentException("required parameter is missed");
+        }
     }
 
     @Override
