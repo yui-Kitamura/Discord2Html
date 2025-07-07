5dec0ce8cddde3bb490ce81ba753899b44e5df43
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-07-07 12:36:48 +0900

    batchタスクの追加 cron実行 #15

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/batch/D2HBotRunner.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/batch/D2HBotRunner.java
@@ -1,5 +1,22 @@
 package pro.eng.yui.oss.d2h.batch;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import pro.eng.yui.oss.d2h.botIF.runner.RunArchiveRunner;
 
+@Service
+@EnableScheduling
 public class D2HBotRunner {
+
+    private final RunArchiveRunner archiveRunner;
+    
+    public D2HBotRunner(RunArchiveRunner archiveRunner) {
+        this.archiveRunner = archiveRunner;
+    }
+
+    @Scheduled(cron = "0 0 * * * *")
+    public void runTask() {
+        archiveRunner.run();
+    }
 }
