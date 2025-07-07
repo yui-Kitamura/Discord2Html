6af1a55a3730edc98ea7fd5f6c7c91a9cfd4d207
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-07-07 12:52:30 +0900

    bean参照ループの解消

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/runner/RunArchiveRunner.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/runner/RunArchiveRunner.java
@@ -1,5 +1,6 @@
 package pro.eng.yui.oss.d2h.botIF.runner;
 
+import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
@@ -21,56 +22,74 @@ public class RunArchiveRunner implements IRunner {
     
     private final GuildsDAO guildDao;
     private final ChannelsDAO channelDao;
-    private final DiscordBot bot;
     
-    public RunArchiveRunner(GuildsDAO g, ChannelsDAO ch, DiscordBot bot){
+    public RunArchiveRunner(GuildsDAO g, ChannelsDAO ch){
         this.guildDao = g;
         this.channelDao = ch;
-        this.bot = bot;
     }
     
     public void run(Member member, List<OptionMapping> options){
         //TODO run make archive file
         
         member.getJDA().getPresence().setPresence(OnlineStatus.ONLINE, DiscordBot.working);
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+
+        loop: for(OptionMapping om : options) {
+            if("target".equals(om.getName())) {
+                String channelName = om.getAsString();
+                for(GuildMessageChannel channel : member.getGuild().getTextChannels()) {
+                    if (channelName.equals(channel.getName())) {
+                        run(channel);
+                        break loop;
+                    }
+                }
+            }
         }
+
         member.getJDA().getPresence().setPresence(OnlineStatus.IDLE, DiscordBot.idle);
     }
     
     public void run(){
         final int now = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
         List<Guilds> allGuilds = guildDao.selectAll();
-        for(Guilds guilds : allGuilds) {
+        loop: for(Guilds guilds : allGuilds) {
             for(RunsOn on : guilds.getRunsOn()) {
                 if(on.getValue() == now) {
                     List<Channels> chs = channelDao.selectAllInGuild(guilds.getGuildId());
                     for(Channels ch : chs) {
-                        run(ch);
+                        run((GuildMessageChannel) ch);
+                        break loop;
                     }
                 }
             }
         }
     }
     
-    private void run(Channels target){
-        Guild guildIn = bot.jda().getGuildById(target.getGuidId().getValue());
-        if(guildIn == null){
-            System.err.println("Guild not found: "+ target.getGuidId());    
+    private void run(JDA jda, Channels target) {
+        Guild guildIn = jda.getGuildById(target.getGuidId().getValue());
+        if (guildIn == null) {
+            System.err.println("Guild not found: " + target.getGuidId());
             return;
         }
         GuildMessageChannel channel = guildIn.getChannelById(GuildMessageChannel.class, target.getChannelId().getValue());
-        if(channel == null) {
+        if (channel == null) {
             System.err.println("Channel not found: " + target.getChannelId());
             return;
         }
         
+        run(channel);
+    }
+    
+    private void run(GuildMessageChannel channel){
+        
         channel.sendMessage("This channel is archive target. Start >>>").queue();
         
         //TODO implement archive
+        
+        try {
+            Thread.sleep(1234);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         
         channel.sendMessage("archive created. task end <<<").queue();
     }
