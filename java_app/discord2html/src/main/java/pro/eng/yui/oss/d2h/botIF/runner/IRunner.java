c1e3d0243ac91b43a3ad2abd1c019c24cfdf6ddb
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-07-07 22:02:58 +0900

    utilの作成

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/runner/IRunner.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/runner/IRunner.java
@@ -1,8 +1,25 @@
 package pro.eng.yui.oss.d2h.botIF.runner;
 
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+
+import java.util.List;
+
 /** marker for command runner */
 public interface IRunner {
     
     /** 終了後replyするメッセージ */
     String afterRunMessage();
+    
+    default OptionMapping get(List<OptionMapping> options, String name){
+        for(OptionMapping op : options) {
+            if (op.getName().equalsIgnoreCase(name)) {
+                return op;
+            }
+        }
+        return null;
+    }
+    
+    default boolean isNull(Object obj){
+        return obj == null;
+    }
 }
