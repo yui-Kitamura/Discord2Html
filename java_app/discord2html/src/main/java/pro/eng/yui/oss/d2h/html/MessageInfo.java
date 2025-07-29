34ddd986d25e6a862d8fde7522de9e8a97674e99
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-07-30 05:39:17 +0900

    データクラスに基づくtemplate構成に修正

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/html/MessageInfo.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/html/MessageInfo.java
@@ -2,17 +2,21 @@ package pro.eng.yui.oss.d2h.html;
 
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageReaction;
-import org.springframework.beans.factory.annotation.Autowired;
 import pro.eng.yui.oss.d2h.db.dao.UsersDAO;
 import pro.eng.yui.oss.d2h.db.field.GuildId;
 import pro.eng.yui.oss.d2h.db.field.UserId;
 import pro.eng.yui.oss.d2h.db.model.Users;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.TimeZone;
 
 public class MessageInfo {
-    
-    private final UsersDAO usersDao;
+
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+    static{
+        DATE_FORMAT.setTimeZone(TimeZone.getTimeZone("Asia/Tokyo"));
+    }
     
     private final String contentRaw;
     public String getContentRaw() {
@@ -22,6 +26,11 @@ public class MessageInfo {
     private final Users userInfo;
     public Users getUserInfo(){
         return this.userInfo;
+    }
+    
+    private final String createdTimestamp;
+    public String getCreatedTimestamp(){
+        return this.createdTimestamp;
     }
     
     private final List<Message.Attachment> attachments;
@@ -41,8 +50,7 @@ public class MessageInfo {
     }
     
     public MessageInfo(Message msg, UsersDAO usersDao){
-        this.usersDao = usersDao;
-
+        this.createdTimestamp = DATE_FORMAT.format(msg.getTimeCreated());
         GuildId guildId = new GuildId(msg.getGuild());
         UserId userId = new UserId(msg.getAuthor());
         this.userInfo =  usersDao.select(guildId, userId);
