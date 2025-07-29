3585ab237bd274a89de3dc9ee519547c61c637e4
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-07-30 00:58:20 +0900

    データの初期化

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/html/MessageInfo.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/html/MessageInfo.java
@@ -1,4 +1,14 @@
 package pro.eng.yui.oss.d2h.html;
 
+import net.dv8tion.jda.api.entities.Message;
+
 public class MessageInfo {
+    
+    public MessageInfo(Message msg){
+        msg.getContentRaw();
+        msg.getMember();
+        msg.getContentRaw();
+        msg.getAttachments();
+        msg.getReactions();
+    }
 }
