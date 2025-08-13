1040d5133567414146c73bf1cbc612f3e2370cd9
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-08-13 15:53:46 +0900

    未使用メソッドの削除

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/github/GitHubService.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/github/GitHubService.java
@@ -30,50 +30,6 @@ public class GitHubService {
     }
 
     /**
-     * Push HTML file to GitHub repository
-     * 
-     * @param htmlFilePath Path to the HTML file to push
-     * @throws Exception If any error occurs during the push process
-     */
-    public void pushHtmlToGitHub(Path htmlFilePath) throws Exception {
-        if (!Files.exists(htmlFilePath)) {
-            throw new IOException("HTML file does not exist: " + htmlFilePath);
-        }
-
-        String repoDir = gitConfig.getLocal().getDir();
-        File repoDirFile = new File(repoDir);
-        if (!repoDirFile.exists() || !repoDirFile.isDirectory()) {
-            repoDirFile.mkdirs();
-        }
-        // repoの確実な初期化
-        gitUtil.ensureRepoInitialized(gitConfig.getRepo().getUrl());
-
-        String dateDir = GitHubConsts.DATE_FORMAT.format(new Date());
-        File targetDir = new File(repoDirFile, GitHubConsts.ARCHIVES_DIR + dateDir);
-        targetDir.mkdirs();
-
-        // Copy HTML file to the repository
-        File targetFile = new File(targetDir, htmlFilePath.getFileName().toString());
-        Files.copy(htmlFilePath, targetFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
-        // Add, commit, and push the file
-        List<String> filesToAdd = new ArrayList<>();
-        filesToAdd.add(getRelativePath(repoDirFile, targetFile));
-
-        try {
-            gitUtil.fetch();
-            gitUtil.pullRebase();
-
-            // Add, commit, and push
-            gitUtil.add(filesToAdd);
-            gitUtil.commit(GitHubConsts.COMMIT_PREFIX + htmlFilePath.getFileName());
-            gitUtil.push();
-        } catch (Exception e) {
-            throw new Exception("Failed to push HTML file to GitHub", e);
-        }
-    }
-
-    /**
      * Push multiple HTML files to GitHub repository in a single operation
      * 
      * @param htmlFilePaths List of paths to the HTML files to push
