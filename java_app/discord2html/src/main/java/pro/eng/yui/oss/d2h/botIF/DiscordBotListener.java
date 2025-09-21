ee2720fd384b3205946e3149ae3df5b43fa114f4
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-09-22 05:40:24 +0900

    wk #121 ポリシーの通知

--- java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordBotListener.java
+++ java_app/discord2html/src/main/java/pro/eng/yui/oss/d2h/botIF/DiscordBotListener.java
@@ -4,31 +4,36 @@ import net.dv8tion.jda.api.OnlineStatus;
 import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
-import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
-import net.dv8tion.jda.api.events.channel.update.GenericChannelUpdateEvent;
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.role.RoleCreateEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import pro.eng.yui.oss.d2h.consts.StringConsts;
+import pro.eng.yui.oss.d2h.github.GitUtil;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Component
 public class DiscordBotListener extends ListenerAdapter {
     
     private final DiscordBotUtils bot;
-    
+    private final Set<String> notifiedOnce; // key: guildId:userId
+    private final GitUtil gitUtil;
+
     @Autowired
-    public DiscordBotListener(DiscordBotUtils bot){
+    public DiscordBotListener(DiscordBotUtils bot, GitUtil gitUtil){
         this.bot = bot;
+        this.notifiedOnce = ConcurrentHashMap.newKeySet();
+        this.gitUtil = gitUtil;
     }
 
     @Override
@@ -60,6 +65,20 @@ public class DiscordBotListener extends ListenerAdapter {
         if (adminRole.size() > 2 && event.getRole().getName().equalsIgnoreCase(StringConsts.ADMIN_ROLE)) {
             event.getRole().delete().reason("duplicate").queue();
         }
+    }
+
+    @Override
+    public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event) {
+        bot.registerUserInfoToDB(event.getMember());
+        try {
+            final String msg = event.getMember().getAsMention() + " " +
+                    "【お知らせ】このサーバーでは、投稿内容やリアクションがアーカイブとして記録・公開される場合があります。" +
+                    "詳しくは、[アーカイブ運用ポリシー]("+ gitUtil.getPagesUrlSafe() +"/tos.html)をご確認ください。\n" +
+                    "発言内容のアーカイブ記録に同意しない場合はコマンド `/d2h optout`をご利用ください";
+            event.getMember().getDefaultChannel().asTextChannel()
+                    .sendMessage(msg)
+                    .queue();
+        }catch(Throwable e){ e.printStackTrace(); }
     }
 
 }
